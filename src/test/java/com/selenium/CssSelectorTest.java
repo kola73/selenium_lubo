b01636da4fa05b6c0de3a1f3bc3f482d95bf2436
kola73 <282156710@qq.com>
2021-10-29 00:43:47 +0800

    css，xpath定位

---
+++ src/test/java/com/selenium/CssSelectorTest.java
@@ -0,0 +1,11 @@
+package com.selenium;
+
+/**
+ * 支持appium，但是appium原生控件不支持css selector，只支持网页样式定位（webview）
+ * 速度比xpath快，因为是基于样式定位
+ * 常用css selector：
+ * 选择器  .class  例子：.intro        描述:选择class="intro"的所有元素
+ * 选择器：#wd     例子：#firstname    描述：选择id="firstname"的所有元素
+ */
+public class CssSelectorTest {
+}
