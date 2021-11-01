b8780a2d49b95fbf675d1d248ec2a4526dc6fc48
kola73 <282156710@qq.com>
2021-11-01 23:47:41 +0800

    selenium 实战

---
+++ src/main/java/com/selenium/hogwarts/PO_Mode/MainPage.java
@@ -0,0 +1,28 @@
+package com.selenium.hogwarts.PO_Mode;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainPage {
+    private WebDriver driver;
+    private By clickBtn = By.id("su");
+    private By inputBox = By.id("kw");
+
+    public MainPage start() {
+        driver = new ChromeDriver();
+        // 隐式等待：设置一个等待时间，轮询查找元素是否出现，超时未出现就报异常
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.get("https://www.baidu.com");
+        // 返回自己，以便进行链式调用
+        return this;
+    }
+
+    public SearchPage search(String keyword) {
+        driver.findElement(By.id("kw")).sendKeys(keyword);
+        driver.findElement(clickBtn).click();
+        return new SearchPage(driver);
+    }
+}
